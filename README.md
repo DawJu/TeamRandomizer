165ed9a96b413f2d1f28d56670f293ee0783be1e
DawJu <dwdjuszkiewicz@gmail.com>
2024-05-19 01:30:39 +0200

    added reroll, switched to json data

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 A short script for making random teams for Spiral Abyss in Genshin Impact.
 
-`character_list.py` contains a list of characters to choose from.
+`character_list.json` contains a list of characters to choose from.
--- randomizer.py
+++ randomizer.py
@@ -1,13 +1,20 @@
+import json
 from random import randint
 from typing import List
-from character_list import char_list
 
 
+def load_char_list(filename: str) -> List[str]:
+    with open(filename, 'r') as file:
+        char_list = json.load(file)
+    return char_list
+
+
+char_list = load_char_list('character_list.json')
 total_char_count = len(char_list)
 chars_to_roll = 8
 
 
-def get_random_char_indices(total: int = total_char_count, n: int = chars_to_roll) -> List:
+def get_random_char_indices(total: int = total_char_count, n: int = chars_to_roll) -> List[int]:
     chars = []
     while len(chars) < n:
         char = randint(1, total)
@@ -16,22 +23,26 @@ def get_random_char_indices(total: int = total_char_count, n: int = chars_to_rol
     return chars
 
 
-def get_char_names() -> List:
+def get_char_names() -> List[str]:
     indices = get_random_char_indices()
     chars = []
     for i in indices:
-        chars.append(char_list[i-1])
+        chars.append(char_list[i - 1])
     return chars
 
 
 def make_teams() -> str:
     pool = get_char_names()
-    half: int = int(chars_to_roll / 2)
+    half = int(chars_to_roll / 2)
     team1 = pool[0:half]
     team2 = pool[half:chars_to_roll]
     teams = f'Team 1: {team1}\nTeam 2: {team2}'
     return teams
 
 
-print(make_teams())
-input('Press Enter to exit...')
+if chars_to_roll <= total_char_count:
+    while True:
+        print(make_teams())
+        user_input = input('[r] - Reroll\t')
+        if user_input.lower() != 'r':
+            break
